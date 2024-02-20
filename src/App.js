9bba8ac7f832290eff2aa3e43f652cffdbd775c4
MarkusIngerslev <markusolsen2004@gmail.com>
2024-02-20 16:10:29 +0100

    Tilføjet så det nu er muligt at se hvilken spillers tur det er samt hvem der har vundet!

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ export default function Board() {
     const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
+    // Add a handleClick function to the Board component
     function handleClick(i) {
-        if (squares[i]) return;
+        if (squares[i] || calculateWinner(squares)) return;
 
         const nextSquares = squares.slice();
         if (xIsNext) {
@@ -25,8 +26,18 @@ export default function Board() {
         setXIsNext(!xIsNext);
     }
 
+    // Display which player has won the game
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -44,4 +55,24 @@ export default function Board() {
             </div>
         </>
     );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
